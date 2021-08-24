7a105c0a4487c7365d4f7f840aca77135dcdc026
PR Aiman <58582776+praiman99@users.noreply.github.com>
2021-08-24 16:53:01 +0800

    Add files via upload

---
+++ Files/WG/del-wg.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+white='\e[0;37m'
+NC='\e[0m'
+
+MYIP=$(curl -s ipinfo.io/ip);
+clear
+source /etc/wireguard/params
+	NUMBER_OF_CLIENTS=$(grep -c -E "^### Client" "/etc/wireguard/$SERVER_WG_NIC.conf")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo -e "\e[1;31m You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo ""
+	echo -e "\e[0;37m Select the existing client you want to remove"
+	echo -e "\e[0;37m Press CTRL+C to return"
+	echo -e "\e[1;31m[*][*][*]===============================[*][*][*]"
+	echo -e "\e[0;37m     No  Expired   User"
+	grep -E "^### Client" "/etc/wireguard/$SERVER_WG_NIC.conf" | cut -d ' ' -f 3-4 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+
+	# match the selected number to a client name
+	CLIENT_NAME=$(grep -E "^### Client" "/etc/wireguard/$SERVER_WG_NIC.conf" | cut -d ' ' -f 3-4 | sed -n "${CLIENT_NUMBER}"p)
+	user=$(grep -E "^### Client" "/etc/wireguard/$SERVER_WG_NIC.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+	exp=$(grep -E "^### Client" "/etc/wireguard/$SERVER_WG_NIC.conf" | cut -d ' ' -f 4 | sed -n "${CLIENT_NUMBER}"p)
+
+	# remove [Peer] block matching $CLIENT_NAME
+	sed -i "/^### Client $user $exp/,/^AllowedIPs/d" /etc/wireguard/wg0.conf
+	# remove generated client file
+	rm -f "/home/vps/public_html/$user.conf"
+
+	# restart wireguard to apply changes
+	systemctl restart "wg-quick@$SERVER_WG_NIC"
+	service cron restart
+clear
+echo -e "\e[0;37m Wireguard Account Deleted Successfully"
+echo -e "\e[1;31m[*][*][*]==========================[*][*][*]"
+echo -e "\e[0;37m Client Name : $user"
+echo -e "\e[0;37m Expired  On : $exp"
+echo -e "\e[1;31m[*][*][*]==========================[*][*][*]"
+echo ""
+read -sp " Press ENTER to go back"
+echo ""
+menu
---
+++ Files/WG/menu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#Script by : PR Aiman
+
+red='\e[1;31m'
+green='\e[0;32m'
+white='\e[0;37m'
+NC='\e[0m'
+
+echo -e "\e[0m                                                   "
+echo -e "\e[1;31m     [*][*][*]======================================[*][*][*]"
+echo -e "\e[0m                                                   "
+echo -e "\e[0;37m                  AutoScriptVPN-WG by PR Aiman"
+echo -e "\e[0m                                                   "
+echo -e "\e[0;37m                  [1] Create Account Wireguard"
+echo -e "\e[0;37m                  [2] Delete Account Wireguard"
+echo -e "\e[0;37m                  [3] Renew Account Wireguard"
+echo -e "\e[0;37m                  [4] Change Timezone"
+echo -e "\e[0;37m                  [x] Exit"
+echo -e "\e[0m                                                   "
+read -p "                Select From Options [1-4 or x] :  " menu
+echo -e "\e[0m                                                   "
+echo -e "\e[1;31m     [*][*][*]======================================[*][*][*]"
+clear
+case $menu in
+		1)
+		clear
+		add-wg
+		exit
+		;;
+		2)
+		clear
+		del-wg
+		exit
+		;;
+		3)
+		clear
+		renew-wg
+		exit
+		;;
+		4)
+		clear
+		change_timezone
+		exit
+		;;
+		x)
+		clear
+		exit
+		;;
+	esac
